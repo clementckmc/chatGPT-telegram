e2e854356411822b48f6d9593345f032212a8728
Clement Choi <clementchoi1997@gmail.com>
2023-04-08 23:36:35 +0900

    feat: openAI setting

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import asyncio
 import os
 import telegram
 from dotenv import load_dotenv
+import openai
 
 load_dotenv()
 
+# telegram
 BOT_TOKEN = os.environ['BOT_TOKEN']
+
+# openai
+openai.organization = "org-xAfG7yH1ydHAjgbRGCeSFfrO"
+openai.api_key = os.environ['OPENAI_API_KEY']
+MODEL = "text-davinci-003"
 
 async def main():
     bot = telegram.Bot(BOT_TOKEN)
     async with bot:
-        print(await bot.get_me())
+        await bot.send_message(text="Hi me!", chat_id=766116213)
 
 
 if __name__ == '__main__':
