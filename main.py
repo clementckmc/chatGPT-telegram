05eb6c08bafccbeaa6ae37fd2e51dda358b9b8a1
Clement Choi <clementchoi1997@gmail.com>
2023-04-22 23:12:32 +0900

    get ai response and send message functions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
 import os
-import telegram
+import logging
+import requests
+from telegram import Update, Bot
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 import openai
 
@@ -14,11 +17,38 @@ openai.organization = "org-xAfG7yH1ydHAjgbRGCeSFfrO"
 openai.api_key = os.environ['OPENAI_API_KEY']
 MODEL = "text-davinci-003"
 
-async def main():
-    bot = telegram.Bot(BOT_TOKEN)
-    async with bot:
-        await bot.send_message(text="Hi me!", chat_id=766116213)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="This is chatGPT. You can ask me anything!")
+
+def getResponse(prompt):
+    response = requests.post(
+        'https://api.openai.com/v1/completions',
+        headers={'Authorization': "Bearer {openai.api_key}"},
+        json={'model': MODEL, 'prompt': prompt, 'max_tokens': 128, 'temperature': 0.5 }  )
+
+    return response.json()
+
+def sendMessage(chat_id, message_id, message):
+    data = {
+        'chat_id': chat_id,
+        'text': message,
+        'reply_to_message_id': message_id
+    }
+
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+
+    application.add_handler(start_handler)
+
+    application.run_polling()
